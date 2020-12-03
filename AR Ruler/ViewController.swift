5f4c3a1b4703ef819a3c9251b819b6264fbc159e
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2020-12-03 16:48:19 -0600

    Creating 3D text for measurements

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -12,6 +12,7 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var dotNodes = [SCNNode]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +47,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let results = sceneView.session.raycast(query)
             
             guard let hitTestResult = results.first else {
-                print("No surface found<")
+                print("No surface found")
                 return
             }
             addDot(at: hitTestResult)
@@ -63,5 +64,37 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         dotNode.position = SCNVector3(location.worldTransform.columns.3.x, location.worldTransform.columns.3.y, location.worldTransform.columns.3.z)
         
         sceneView.scene.rootNode.addChildNode(dotNode)
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculateDistance()
+        }
+    }
+    
+    func calculateDistance() {
+        let start = dotNodes[0].position
+        let end = dotNodes[1].position
+        
+        let a = end.x - start.x
+        let b = end.y - start.y
+        let c = end.z - start.z
+        
+        // distance = √ (x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2
+        
+        let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        
+        updateText(text: "\(abs(distance) * 100) cm", at: end)
+        
+    }
+    
+    func updateText(text: String, at position: SCNVector3) {
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.systemRed
+         
+        let textNode = SCNNode(geometry: textGeometry)
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
     }
 }
